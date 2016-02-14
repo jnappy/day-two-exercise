1f659ac8ea9e89d8eb5a7cf734ba2e391cf43660
Jay <jpnappy@gmail.com>
2016-02-14 10:21:32 -0500

    created a morning exercis

---
+++ no-vowels.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+# write a function called `without_vowles` that accepts one parameter called `favorite_word`
+# within the function, create a variable called `word_without_vowels` and set it equal to the `favorite_word` without vowels (hint:  delete or replace the vowels with an empty string) 
+# then, the function should make the computer say aloud "FAVORITE_WORD sounds like WORD_WITHOUT_VOWELS when it doens't have vowels"
+# (you can do this a few ways - I recommend using `.gsub!(` and `system("say('...')")
+
+# ask the user for their favorite word
+# store their word in a variable called `user_favorite_word`
+
+# call the functions with the the user's word
